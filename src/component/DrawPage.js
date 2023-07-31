7841d95765398375a46820eca3354a28b8d01cd0
Carter Figgins <Carter.Figgins@atomicjolt.com>
2023-07-31 15:50:36 -0600

    Prev Next button for drawings

--- src/component/DrawPage.js
+++ src/component/DrawPage.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Drawing from "./Drawing";
 
 export default function DrawPage() {
+  const [drawIndex, setDrawIndex] = useState(0);
   const triangleURL = "https://trinket.io/embed/python/411b16cb81";
   const treeURL = "https://trinket.io/embed/python/04dad69445";
   const spikeURL = "https://trinket.io/embed/python/998b5c1e8a";
+
+  const drawings = [treeURL, triangleURL, spikeURL];
+  const isEndOfDrawings = drawIndex + 1 >= drawings.length;
+  const isStartOfDrawings = drawIndex <= 0;
+
+  console.log(drawIndex)
+  console.log(isStartOfDrawings)
 
   return (
     <div>
@@ -21,7 +30,16 @@ export default function DrawPage() {
           </a>{" "}
           library to draw the art below.
         </p>
-        <a
+        
+        <div>
+          <button
+            disabled={isStartOfDrawings}
+            className="nav-btn"
+            onClick={() => setDrawIndex(drawIndex - 1)}
+          >
+            Prev
+          </button>
+          <a
           className="link-button"
           target="_blank"
           rel="noopener noreferrer"
@@ -29,10 +47,16 @@ export default function DrawPage() {
         >
           View Code
         </a>
+          <button
+            disabled={isEndOfDrawings}
+            className="nav-btn"
+            onClick={() => setDrawIndex(drawIndex + 1)}
+          >
+            Next
+          </button>
+        </div>
         <div className="drawing-boxes">
-          <Drawing drawURL={treeURL} />
-          <Drawing drawURL={spikeURL} />
-          <Drawing drawURL={triangleURL} />
+          <Drawing drawURL={drawings[drawIndex]} />
         </div>
       </div>
     </div>
