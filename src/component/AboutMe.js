fd7d971799e1e76efc77216802c5c6eb59c3b2e4
Carter Figgins <Carter.Figgins@atomicjolt.com>
2023-08-10 23:20:59 -0600

    added world select

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -1,4 +1,21 @@
+import StarWarsIntro from "./introductions/StarWarsIntro";
+import PokemonIntro from "./introductions/PokemonIntro";
+import { useState } from "react";
+
 export default function AboutMe() {
+
+  const [world, setWorld] = useState(StarWarsIntro)
+
+  const worldChange = (selectedWorld) => {
+    if (selectedWorld.target.value === "star-wars") {
+      setWorld(StarWarsIntro)
+    } else if (selectedWorld.target.value === "pokemon") {
+      setWorld(PokemonIntro)
+    }
+  }
+
+
+
   return (
     <div className="content-column">
       <div className="about-me-title">
@@ -6,28 +23,11 @@ export default function AboutMe() {
         <p className="content-subtitle">The Computing Fig</p>
         <p className="content-subtitle-sm">Hello, welcome to my website!</p>
       </div>
-      <div className="content-body about-me-body">
-        <div className="content-body__text fade-in-from-top">
-          <p>
-          In a galaxy not far away...🌎 The force is strong with me as I wield my lightsaber of full stack development at <b><a href="https://www.atomicjolt.com/" target="blank" className="link-in-text">Atomic Jolt</a>!</b> 💻⚔️ A Jedi master in <b>JavaScript, Ruby, Python, and Go</b>, I stand ready to defend the digital realm from the dark side! 🚀🌟
-          </p>
-          <p>
-            With the wisdom of the Jedi Council, I harness the power of <b>React, GraphQL, Ruby On Rails, Vue, ORM tools, and Git.</b> The force flows through these tools, guiding me in my web development quests! 🔮💫
-          </p>
-          {/* <p>
-            But the battle doesn't end there! When I'm not taking on Sith-level coding challenges, I become a daring starfighter, navigating the treacherous trails of mountain biking with Jedi-like skill! 🚴‍♂️🌲
-          </p> */}
-          <p>
-            Amidst these cosmic endeavors, I treasure my time with my dear wife and young Padawans, for family bonds are stronger than any lightsaber crystal! ❤️👨‍👩‍👧‍👦
-          </p>
-          <p>
-            So, brave rebels, join me on this epic journey. Together, we shall bring balance to the digital force, one line of code at a time! <b>May the code be with you!</b>⚡
-          </p>
-        </div>
-        <div className="image-container fade-in">
-          <img className="about-me-img" alt={"me"} src={"/images/carter_jedi.png"} />
-        </div>
-      </div>
+      <select className="world-select" name="worlds" id="worlds" onChange={worldChange}>
+        <option value="star-wars">Star Wars</option>
+        <option value="pokemon">Pokémon</option>
+      </select>
+      {world}
       <div className="content-body about-me-footer">
         <p className="fade-in">*Image and text enhanced by AI</p>
       </div>
---
+++ src/component/introductions/PokemonIntro.js
@@ -0,0 +1,22 @@
+export default function PokemonIntro() {
+  return (
+    <div className="content-body about-me-body">
+      <div className="content-body__text fade-in-from-top">
+        <p>
+          In the coding arena, I'm a Pokémon Trainer at <b><a href="https://www.atomicjolt.com/" target="blank" className="link-in-text">Atomic Jolt</a>!</b> I
+          train with the versatile Pikachu, ⚡️<b>JavaScript</b>⚡️, who zaps through challenges. With 🔥<b>Ruby</b>🔥 the Charizard, I breathe fiery solutions into
+          existence. 🐍<b>Python</b>🐍, the cunning Arbok, dives deep into problems. And 🦫<b>Go</b>🦫, the swift Bidoof, evolves into intricate systems.
+        </p>
+        <p>
+          But my coding journey is far from over! Just as Pikachu electrifies with "Thunderbolt", 🌩️<b>React</b>🌩️ charges my code battles with lightning-fast adaptability! 🧠<b>GraphQL</b>🧠 channels the strategic wisdom of "Mind Reader", mapping my path with precision. 🛡️<b>Ruby On Rails</b>🛡️ transforms into the unyielding shield of "Iron Defense", warding off coding challenges. 🔮 <b>Vue</b>🔮 , akin to the mystic powers of "Future Sight", casts a charming spell on interfaces, weaving a prophecy of innovation.
+        </p>
+        <p>
+          <b>ORM frameworks</b> powers every query, data interaction, and relationship mapping that becomes the silent force that ensures victory in the coding arena. <b>PostgreSQL</b> brings the earth-shattering force of "Earthquake", reshaping my coding landscape. Just as Eevee demonstrates teamwork through "Helping Hand", <b>Git</b> rallies my coding crew with its harmonizing "Symphony of Sync"! 
+        </p>
+      </div>
+      <div className="image-container fade-in">
+        <img className="about-me-img" alt={"me"} src={"/images/carter_pokemon_trainer.jpeg"} />
+      </div>
+    </div>
+  );
+}
