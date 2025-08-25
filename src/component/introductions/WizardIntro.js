12876967c5588d62db99aac84bdf9244b0e24428
Carter Figgins <54256641+CarterFiggins@users.noreply.github.com>
2025-08-25 14:01:33 -0600

    Update WizardIntro.js

--- src/component/introductions/WizardIntro.js
+++ src/component/introductions/WizardIntro.js
@@ -3,7 +3,7 @@ export default function WizardIntro() {
     <div className="content-body about-me-body">
       <div className="content-body__text fade-in-from-top">
         <p>
-          Greetings, fellow wizards and witches! I am a digital spellcaster at the esteemed <b><a href="https://www.atomicjolt.com/" target="blank" className="link-in-text">Atomic Jolt</a></b>, where for four magical years, I have practiced the arcane arts of full-stack development. My enchanted tools and charms bring forth wondrous Learning Management Systems, empowering the pursuit of knowledge across our enchanted realm.
+          Greetings, fellow wizards and witches! I am a digital spellcaster at the esteemed <b><a href="https://www.atomicjolt.com/" target="blank" className="link-in-text">Atomic Jolt</a></b>, where for five magical years, I have practiced the arcane arts of full-stack development. My enchanted tools and charms bring forth wondrous Learning Management Systems, empowering the pursuit of knowledge across our enchanted realm.
         </p>
         <p>
           Within my grimoire of programming spells, you'll find the incantations of Javascript, the reliable Node.js, the sturdy PostgreSQL, the ever-changing React.js, the swift Go, the cloud-bound AWS, the multifaceted Ruby on Rails, and the structured Typescript. Each spell, a testament to my wizarding prowess, conjures solutions to the most intricate of digital challenges.
