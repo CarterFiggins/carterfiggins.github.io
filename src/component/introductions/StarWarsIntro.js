3eacf2edcb02f8ff0fab096316ac3610d5f37f99
Carter Figgins <54256641+CarterFiggins@users.noreply.github.com>
2025-08-25 14:00:54 -0600

    Update StarWarsIntro.js

--- src/component/introductions/StarWarsIntro.js
+++ src/component/introductions/StarWarsIntro.js
@@ -3,7 +3,7 @@ export default function StarWarsIntro() {
     <div className="content-body about-me-body">
       <div className="content-body__text fade-in-from-top">
         <p>
-          In the vast galaxy of software development, I am a Jedi Knight serving the revered <b><a href="https://www.atomicjolt.com/" target="blank" className="link-in-text">Atomic Jolt</a></b>. For four rotations around our twin suns, I have honed my skills as a full-stack developer, forging powerful tools for Learning Management Systems, akin to lightsabers in the hands of knowledge seekers.
+          In the vast galaxy of software development, I am a Jedi Knight serving the revered <b><a href="https://www.atomicjolt.com/" target="blank" className="link-in-text">Atomic Jolt</a></b>. For five rotations around our twin suns, I have honed my skills as a full-stack developer, forging powerful tools for Learning Management Systems, akin to lightsabers in the hands of knowledge seekers.
         </p>
         <p>
           My arsenal includes the versatile Javascript, the steadfast Node.js, the resilient PostgreSQL, the dynamic React.js, the swift Go, the cloud-based AWS, the multifaceted Ruby on Rails, and the structured Typescript. Each language and tool, a Force power that I wield with precision and grace.
